ceac861f98924819e7ea31bc07c1a05a37cdfcc7
Saad Zafar <saad.zafar.7223@gmail.com>
2016-12-05 14:46:24 +0800

    Initial release

---
+++ proxy.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env nodejs
+const admin = require('firebase-admin'),
+	express = require('express'),
+	httpProxy = require('http-proxy'),
+	apiProxy = httpProxy.createProxyServer(),
+	serviceAccount = require(''), //service account key JSON file
+	endpoint = '', //proxy destination
+	port = 8080, //port on which application listens
+	token_key = ''; //key of the token header
+
+//initialize the the Firebase Admin SDK
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://<DATABASE_NAME>.firebaseio.com" //replace <DATABASE_NAME> with the name of your Firebase Database
+});	
+	
+var app = express(); //initialize Express
+
+app.get('/*', function(req, res) { //GET API
+	authenticate(req, res);
+});
+
+app.put('/*', function(req, res) { //PUT API
+	authenticate(req, res);
+});
+
+app.post('/*', function(req, res) { //POST API
+	authenticate(req, res);
+});
+
+app.delete('/*', function(req, res) { //DELETE API
+	authenticate(req, res);
+});
+
+app.listen(port, 'localhost'); //initialize listener for requests
+
+//authenticates the token using Firebase Admin SDK
+function authenticate (req, res) {
+	var idToken = req.headers[token_key]; //grab the token from the http header
+	
+	if (idToken) {
+		//uses the Firebase Admin SDK to decode the token and get the UID
+		admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+			var uid = decodedToken.uid;
+		
+			console.log(uid, decodedToken.name);
+			apiProxy.web(req, res, {target: endpoint});
+		}).catch(function(error) {
+			console.log('Invalid token or token expired');
+			res.send('You have provided an invalid token or your token has expired\n')
+		});
+	}
+	else { 
+		console.log('no token provided');
+		res.send('Please provide a token\n');
+	 }
+}
